85d6a98d9c138dd327736a8ac4ecdc29ebc428dd
Yuriy Bandura <bandurayv@gmail.com>
2024-08-14 18:11:59 +0300

    Add_New_Code_1

--- main.py
+++ main.py
@@ -7,26 +7,42 @@ class WARRIOR():
 
 
     def sleep(self):
-        return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
+        #return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
         print(f'{self.name} лег спать')
         self.endurance += 2
 
     def eat(self):
-        return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
+        #return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
         print(f'{self.name} ест')
         self.power += 1
 
     def hit(self):
-        return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
+        #return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
         print(f'{self.name} ударил')
         self.endurance -= 1
 
     def wolk(self):
-        return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
+        #return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
         print(f'{self.name} гуляет')
         self.endurance -= 1
 
     def info(self):
         return f'{self.name} {self.power} {self.endurance} {self.hair_color}'
-        print(f'имя -{self.name}, сила - {self.power}, выносливость - {self.endurance}, цвет волос {self.hair_color}')
-        
+        print(f'имя - {self.name}, сила - {self.power}, выносливость - {self.endurance}, цвет волос - {self.hair_color}')
+
+
+war1 = WARRIOR('Варвар', 100, 50, 'черный')
+war2 = WARRIOR('Викинг', 150, 100, 'седой')
+
+print(war1.info())
+print(war2.info())
+
+war1.sleep()
+war2.eat()
+war1.hit()
+war2.wolk()
+war1.eat()
+
+print(war1.info())
+print(war2.info())
+
